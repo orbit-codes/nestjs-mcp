62f1ecd36135c705427d9d97fdd96f3a15c1aa48
Segfault <5221072+Segfaultd@users.noreply.github.com>
2025-03-20 20:28:07 +0100

    Format json files

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,15 @@
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  testRegex: '.*\\.spec\\.ts$',
-  transform: {
-    '^.+\\.(t|j)s$': 'ts-jest',
-  },
-  collectCoverageFrom: ['**/*.(t|j)s'],
-  coverageDirectory: './coverage',
-  moduleFileExtensions: ['js', 'json', 'ts'],
-  coveragePathIgnorePatterns: [
-    'node_modules',
-    'dist',
-  ],
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    testRegex: '.*\\.spec\\.ts$',
+    transform: {
+        '^.+\\.(t|j)s$': 'ts-jest',
+    },
+    collectCoverageFrom: ['**/*.(t|j)s'],
+    coverageDirectory: './coverage',
+    moduleFileExtensions: ['js', 'json', 'ts'],
+    coveragePathIgnorePatterns: [
+        'node_modules',
+        'dist',
+    ],
 }; 
